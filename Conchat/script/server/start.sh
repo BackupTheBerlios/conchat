7dc91fd42c07ce3a7082ec9f54d0cef52d6287b9
setcool <setcool>
2006-12-24 11:30:16 +0000

    Make some bugfixes in the build.xml and start scripts

--- Conchat/script/server/start.sh
+++ Conchat/script/server/start.sh
@@ -3,8 +3,8 @@
 vm=`which java`
 
 port=8080
-config=data/mapping.xml
+config=data/config.xml
 read -p "Server-Admin-Password: " password
-$vm -cp lib/chatserver.jar:lib/jython.jar:data/plugins com.telekom.bs.chatserver.server.Application $port $config $password
+$vm -cp lib/server.jar:lib/jython.jar:data/plugins de/conchat/server/Application $port $config $password
 
 #$vm -cp lib/chatserver.jar:lib/jython.jar:data/plugins com.telekom.bs.chatserver.server.Application $@
